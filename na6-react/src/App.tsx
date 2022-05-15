27a9f8118881293a7a0580352ba89cd4a9f5bc13
Kuba429 <jakubzawadzki429@gmail.com>
2022-05-15 17:35:08 +0200

    bootstrap importowany przez cdn, male zmiany

--- na6-react/src/App.tsx
+++ na6-react/src/App.tsx
@@ -1,9 +1,10 @@
+import { Container } from "react-bootstrap";
 import { Menu } from "./components/Menu";
 import dane from "./dane";
 
 function App() {
     return (
-        <div className="App">
+        <div className="App container">
             <h1>Wielcy Naukowcy</h1>
             <Menu dane={dane} />
         </div>
--- na6-react/src/main.tsx
+++ na6-react/src/main.tsx
@@ -1,4 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
