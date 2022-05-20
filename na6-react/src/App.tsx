d342abed78cd479b2d62412ec474bfdc0415dbe4
Kuba429 <jakubzawadzki429@gmail.com>
2022-05-20 20:05:15 +0200

    footer

--- na6-react/src/App.tsx
+++ na6-react/src/App.tsx
@@ -1,15 +1,19 @@
 import { Container } from "react-bootstrap";
+import { Footer } from "./components/Footer";
 import { Menu } from "./components/Menu";
 import { Tabela } from "./components/Tabela";
 import dane from "./dane";
 
 function App() {
     return (
-        <Container className="App">
-            <h1>Wielcy Naukowcy</h1>
-            <Menu dane={dane} />
-            <Tabela dane={dane} />
-        </Container>
+        <div className="App d-flex flex-column">
+            <Container className="my-5">
+                <h1>Wielcy Naukowcy</h1>
+                <Menu dane={dane} />
+                <Tabela dane={dane} />
+            </Container>
+            <Footer />
+        </div>
     );
 }
 
