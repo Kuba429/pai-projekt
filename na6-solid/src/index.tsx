b6a17e748b41950206ab3dbfa7ce29ead0fb0c8c
Kuba429 <jakubzawadzki429@gmail.com>
2022-05-28 18:21:06 +0200

    ikony bootstrap

--- na6-solid/src/index.tsx
+++ na6-solid/src/index.tsx
@@ -1,5 +1,6 @@
 /* @refresh reload */
 import "bootstrap/scss/bootstrap.scss";
+import "bootstrap-icons/font/bootstrap-icons.scss";
 import "./index.scss";
 import { render } from "solid-js/web";
 import { Router } from "solid-app-router";
