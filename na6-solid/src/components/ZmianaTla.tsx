8d9248d8405e294559ddefb087dd4b99d4b922d7
Kuba429 <jakubzawadzki429@gmail.com>
2022-05-29 20:12:55 +0200

    kolor tla zapisany w localStorage

--- na6-solid/src/components/ZmianaTla.tsx
+++ na6-solid/src/components/ZmianaTla.tsx
@@ -1,5 +1,7 @@
 const kolory = ["white", "yellow", "blue", "green", "aqua"];
 export const ZmianaTla = () => {
+    let zapisanyKolor: string = localStorage.getItem("kolorTla") ?? "white";
+    zmienKolor(zapisanyKolor);
     return (
         <div class="zmiana-tla container d-flex justify-content-end gap-2 my-2">
             {kolory.map((kolor) => {
@@ -8,7 +10,7 @@ export const ZmianaTla = () => {
                         <input
                             onInput={() => zmienKolor(kolor)}
                             class="btn-check"
-                            checked={kolor == "white"}
+                            checked={kolor == zapisanyKolor}
                             type="radio"
                             name="kolor"
                             id={kolor}
@@ -27,5 +29,6 @@ export const ZmianaTla = () => {
 };
 
 const zmienKolor = (kolor: string) => {
+    localStorage.setItem("kolorTla", kolor);
     document.body.dataset.kolor = kolor;
 };
